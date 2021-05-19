de7e344da56209136ebed2fd3189799c41acaba3
Nexterbyby <jenseichenberger@gmail.com>
2021-05-19 15:22:16 +0200

    Do Medium

--- src/main/java/ch/bbw/db_tables/Medium.java
+++ src/main/java/ch/bbw/db_tables/Medium.java
@@ -1,5 +1,26 @@
 package ch.bbw.db_tables;
 
-public class Medium {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@Entity
+@Table(name="medium")
+public class Medium implements Serializable {
+
+    @Id
+    @Column(name = "medium_id", unique = true)
+    private int id;
+
+    @Column(name="name")
+    public String name;
+
+    @Column(name="price")
+    public double price;
+
+    public Medium(){
+    }
 
 }
