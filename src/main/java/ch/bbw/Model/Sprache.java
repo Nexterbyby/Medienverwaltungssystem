d5def19b2c0f23995a92fbdc33e892dc905391ff
Nghia Dao <nghiadao2002@gmail.com>
2021-05-26 14:38:58 +0200

    Sprache Model wurde erstellt und programmiert

---
+++ src/main/java/ch/bbw/Model/Sprache.java
@@ -0,0 +1,60 @@
+package ch.bbw.Model;
+
+
+
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @Author Nghia Dao
+ *
+ */
+
+
+@Entity
+@Table(name = "sprache")
+@NamedQuery(name ="Sprache.findAll", query = "Select e FROM Sprache e")
+public class Sprache {
+    @Id
+    @Column(name = "sprache_id", unique = true)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int sprache_id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Sprache() {
+    }
+
+    public Sprache(int sprache_id, String name) {
+        this.sprache_id = sprache_id;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Sprache{" +
+                "sprache_id=" + sprache_id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+
+    public int getSprache_id() {
+        return sprache_id;
+    }
+
+    public void setSprache_id(int sprache_id) {
+        this.sprache_id = sprache_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
