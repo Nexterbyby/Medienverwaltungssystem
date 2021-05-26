a7fdb87ce54d0903d5d2207ada77df8ff7602fcc
Nghia Dao <nghiadao2002@gmail.com>
2021-05-26 14:51:11 +0200

    In Verlag wurde geschrieben

---
+++ src/main/java/ch/bbw/Model/Verlag.java
@@ -0,0 +1,44 @@
+package ch.bbw.Model;
+
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "verlag")
+@NamedQuery(name ="Verlag.findAll", query = "Select e FROM Verlag e")
+public class Verlag {
+
+    @Id
+    @Column(name = "verlag_id", unique = true)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int verlag_id;
+
+    @Column(name = "name")
+    private String name;
+
+    public Verlag() {
+    }
+
+    public Verlag(int verlag_id, String name) {
+        this.verlag_id = verlag_id;
+        this.name = name;
+    }
+
+//Getter and Setter
+
+    public int getVerlag_id() {
+        return verlag_id;
+    }
+
+    public void setVerlag_id(int verlag_id) {
+        this.verlag_id = verlag_id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
