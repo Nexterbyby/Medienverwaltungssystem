af7cee150944578fd28ba3d730084777274b2aa5
Nexterbyby <jenseichenberger@gmail.com>
2021-06-13 21:22:48 +0200

    Do create_game.fxml

--- src/main/java/ch/bbw/CreateGameController.java
+++ src/main/java/ch/bbw/CreateGameController.java
@@ -1,12 +1,55 @@
 package ch.bbw;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
 
 public class CreateGameController {
+
     @FXML
-    private void createGame() throws IOException {
+    private TextField fld1;
+    @FXML
+    private TextField fld2;
+    @FXML
+    private TextField fld3;
+    @FXML
+    private TextField fld4;
+    @FXML
+    private TextField fld5;
+    @FXML
+    private TextField fld6;
+    @FXML
+    private TextField fld7;
+
+
+    @FXML
+    private void createData() throws IOException {
         App.setRoot("mainview");
+    }
+
+    @FXML
+    private void submit(){
+        String [] fields = getFields();
+        for(int i = 0; i < 7; i++){
+            System.out.println(fields[i]);
+        }
+    }
+
+    @FXML
+    private void discard() throws IOException{
+        App.setRoot("mainview");
+    }
+
+    private String [] getFields(){
+        return new String[]{
+                fld1.getText(),
+                fld2.getText(),
+                fld3.getText(),
+                fld4.getText(),
+                fld5.getText(),
+                fld6.getText(),
+                fld7.getText(),
+        };
     }
 }
