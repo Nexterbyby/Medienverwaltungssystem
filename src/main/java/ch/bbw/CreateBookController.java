9c7310db4be97b9d205db0736b0c33156b0cf258
Nghia Dao <nghiadao2002@gmail.com>
2021-06-23 13:25:50 +0200

    Changed "Edit" view and inserted data into ChoiceBox

--- src/main/java/ch/bbw/CreateBookController.java
+++ src/main/java/ch/bbw/CreateBookController.java
@@ -1,9 +1,15 @@
 package ch.bbw;
 
+import ch.bbw.Model.Sprache;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class CreateBookController {
 
--- src/main/java/ch/bbw/UpdateDeleteBookController.java
+++ src/main/java/ch/bbw/UpdateDeleteBookController.java
@@ -1,12 +1,56 @@
 package ch.bbw;
 
 import java.io.IOException;
-import javafx.fxml.FXML;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class UpdateDeleteBookController {
+import ch.bbw.Model.Kaufort;
+import ch.bbw.Model.Sprache;
+import ch.bbw.Model.Typ;
+import ch.bbw.Model.Verlag;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+public class UpdateDeleteBookController implements Initializable {
 
     @FXML
     private void switchToMainview() throws IOException {
         App.setRoot("mainview");
+    }
+
+    @FXML
+    private ChoiceBox<String> csprache = new ChoiceBox<String>();
+    @FXML
+    private ChoiceBox<String> ckaufort = new ChoiceBox<String>();
+    @FXML
+    private ChoiceBox<String> ctyp = new ChoiceBox<String>();
+    @FXML
+    private ChoiceBox<String> cverlag = new ChoiceBox<String>();
+    @FXML
+    private TextField ename, epreis, ekommentar, eseite;
+    @FXML
+    private CheckBox ccurrenttime;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        for (Sprache s:
+                App.db_manager.getAllSprachen()) {
+            csprache.getItems().add(s.toString());
+        }
+        for (Kaufort k: App.db_manager.getAllKauforte()){
+            ckaufort.getItems().add(k.toString());
+        }
+        for (Typ t: App.db_manager.getAllTypen()){
+            ctyp.getItems().add(t.toString());
+        }
+        for (Verlag v: App.db_manager.getAllVerlaege()){
+            cverlag.getItems().add(v.toString());
+        }
+
+
     }
 }
