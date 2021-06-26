8d11a6a1fe6b3e5f78bf7ba11af11609e4b36ad2
Nghia Dao <nghiadao2002@gmail.com>
2021-06-26 23:31:39 +0200

    Create of Medium was created

--- src/main/java/ch/bbw/CreateBookController.java
+++ src/main/java/ch/bbw/CreateBookController.java
@@ -1,8 +1,6 @@
 package ch.bbw;
 
-import ch.bbw.Model.Kaufort;
-import ch.bbw.Model.Sprache;
-import ch.bbw.Model.Verlag;
+import ch.bbw.Model.*;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
@@ -11,9 +9,12 @@ import javafx.scene.control.TextField;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
-public class CreateBookController {
+public class CreateBookController implements Initializable {
 
     @FXML
     private TextField fld1;
@@ -22,13 +23,16 @@ public class CreateBookController {
     @FXML
     private TextField fld3;
     @FXML
-    private ChoiceBox<Sprache> csprache;
+    private ChoiceBox<Sprache> csparche;
     @FXML
     private ChoiceBox<Verlag> cpublisher;
     @FXML
     private ChoiceBox<Kaufort> ckaufort;
     @FXML
+    private ChoiceBox<Typ> ctyp;
+    @FXML
     private TextField fld8;
+
 
 
     @FXML
@@ -37,11 +41,26 @@ public class CreateBookController {
     }
 
     @FXML
-    private void submit(){
+    private void submit() throws IOException{
+        if (currentdate.isSelected()) {
+            LocalDateTime now = LocalDateTime.now();
+            DateTimeFormatter f = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            Timestamp s = Timestamp.valueOf(now.format(f));
+            Medium medium = new Medium( s, fld1.getText(), Double.parseDouble(fld3.getText()),csparche.getValue(), cpublisher.getValue(),
+                    ckaufort.getValue(), ctyp.getValue(), fld8.getText());
+            App.db_manager.saveMedium(medium);
+        } else{
+            Medium medium = new Medium(fld1.getText(), Double.parseDouble(fld3.getText()),csparche.getValue(), cpublisher.getValue(),
+                    ckaufort.getValue(), ctyp.getValue(), fld8.getText());
+            App.db_manager.saveMedium(medium);
+        }
+        /*
         String [] fields = getFields();
         for(int i = 0; i < 8; i++){
             System.out.println(fields[i]);
         }
+         */
+        App.setRoot("mainview");
     }
 
     @FXML
@@ -57,4 +76,20 @@ public class CreateBookController {
         };
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        for (Sprache s:
+                App.db_manager.getAllSprachen()) {
+            csparche.getItems().add(s);
+        }
+        for (Kaufort k: App.db_manager.getAllKauforte()){
+            ckaufort.getItems().add(k);
+        }
+        for (Typ t: App.db_manager.getAllTypen()){
+            ctyp.getItems().add(t);
+        }
+        for (Verlag v: App.db_manager.getAllVerlaege()){
+            cpublisher.getItems().add(v);
+        }
+    }
 }
