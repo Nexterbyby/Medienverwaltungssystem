0af2650faea80e891e4313c086bf820f72045784
Nexterbyby <jenseichenberger@gmail.com>
2021-07-05 21:18:59 +0200

    Add javaDoc comments for TableFormatter.java

--- src/main/java/ch/bbw/Formatter/TableFormatter.java
+++ src/main/java/ch/bbw/Formatter/TableFormatter.java
@@ -10,12 +10,19 @@ import java.util.List;
  * @author Nexterbyby
  * @version 0.0.1 14.06.2021
  * @since 0.0.1 14.06.2021
+ *
+ * Diese Klasse enthält Funktionen die mit Tabellenformatierung zu tun haben.
  */
 public class TableFormatter {
     public TableFormatter(){
         // nothing
     }
 
+    /**
+     * Diese Funktion nimmt Medium Datensätze und formattiert sie in eine Tabelle zur visualisierung.
+     * @param datasets Eine Liste mit Medium Objekten.
+     * @return Gibt eine String zurück im Tabellen Format.
+     */
     public static String formatTableMedium(List<Medium> datasets){
         String out = "";
         out += String.format("%-5s %-60s %-29s %-25s %-15s %-15s %-30s %-15s %-50s %-20s\n",
@@ -44,9 +51,5 @@ public class TableFormatter {
                     m.getGenres());
         }
         return out;
-    }
-
-    public static List<String> formatTableGenre(List<Genre> datasets){
-        return null;
     }
 }
